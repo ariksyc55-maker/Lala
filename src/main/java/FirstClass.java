234113029a882bdafcc88949012baf6b1b8c1612
YaroslavSych <ariksyc55@gmail.com>
2025-12-04 13:18:29 +0300

    add more new method

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,20 +1,13 @@
+import java.util.Scanner;
+
 public class FirstClass {
 
     public static void main(String[] args) {
-        System.out.println("Система расчета штрафов");
-        int carSpeed = 130;
-        System.out.println(carSpeed);
-        int fineFor20to40 = 500;
-        int fineFor40to60 = 1000;
-        int fineFor60to80 = 2000;
-        int fineFor80andmore = 5000;
-        int townSpeed = 60;
-        int overSpeed = carSpeed - townSpeed;
-        if(overSpeed < 20);{
-            System.out.println("Скорость не превышена или превышена не значительно");
+        System.out.println("--- Задание 4: Сумма нечетных чисел от 1 до 100 ---");
+        int oddSum = 0;
+        for (int i = 1; i <= 100; i += 2) {
+            oddSum += i;
         }
-        if(overSpeed >= 20 && overSpeed < 40);{
-            System.out.println("Штраф: " + fineFor20to40);
-        }
+        System.out.println(oddSum);
     }
 }
--- src/main/java/ThirdClass.java
+++ src/main/java/ThirdClass.java
@@ -1,6 +1,9 @@
 public class ThirdClass {
-    public static void main(String[] args){
-        String set = new java.util.BitSet() {{ set(1, 10+1); }}.toString();
-        System.out.append(set, 1, set.length()-1);
-    }
-}
+    public static void main(String[] args) {
+        System.out.println("--- Задание 2: Числа от 1 до 10 ---");
+        System.out.println("Числа от 1 до 10:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println();
+    }}
