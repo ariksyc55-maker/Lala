63c496ab67d88474d1a8628a3f1c7ad3cd951047
YaroslavSych <ariksyc55@gmail.com>
2025-12-04 12:31:15 +0300

    add new method

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,6 +1,20 @@
 public class FirstClass {
 
     public static void main(String[] args) {
-        System.out.println("3");
+        System.out.println("Система расчета штрафов");
+        int carSpeed = 130;
+        System.out.println(carSpeed);
+        int fineFor20to40 = 500;
+        int fineFor40to60 = 1000;
+        int fineFor60to80 = 2000;
+        int fineFor80andmore = 5000;
+        int townSpeed = 60;
+        int overSpeed = carSpeed - townSpeed;
+        if(overSpeed < 20);{
+            System.out.println("Скорость не превышена или превышена не значительно");
+        }
+        if(overSpeed >= 20 && overSpeed < 40);{
+            System.out.println("Штраф: " + fineFor20to40);
+        }
     }
 }
