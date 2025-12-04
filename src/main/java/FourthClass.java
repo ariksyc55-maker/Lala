970792e779777fe1ab2933eef30967a481d28fee
YaroslavSych <ariksyc55@gmail.com>
2025-12-04 13:42:29 +0300

    add more new method

---
+++ src/main/java/FourthClass.java
@@ -0,0 +1,11 @@
+public class FourthClass {
+    public static void main(String[] args) {
+        System.out.println("--- Задание 3: Сумма  чисел от 1 до 100 ---");
+        int oddSum = 0;
+        for (int i = 1; i <= 100; i++) {
+            oddSum += i;
+        }
+        System.out.println(oddSum);
+        }
+}
+
