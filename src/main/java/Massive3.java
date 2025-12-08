0295df077e2fc2f28edfe3bbc9eeba8f5832fe33
User <ariksyc55@gmail.com>
2025-12-08 21:30:40 +0300

    New test

---
+++ src/main/java/Massive3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Massive3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество элементов массива");
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        System.out.println("Введите " + n + " чисел:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();}
+        for (int i = 0; i < numbers.length / 2; i++);
+        System.out.println("Количество значений");
+        System.out.println(numbers.length);
+        int max = numbers[0];
+        for (int i = 1; i < n; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+                System.out.println("Максимальное значение в массиве: " + max);
+
+    }
+}
