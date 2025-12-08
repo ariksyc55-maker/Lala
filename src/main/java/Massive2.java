ab7b760b3089108ecf51de78ea5c1808b65bf917
User <ariksyc55@gmail.com>
2025-12-08 20:44:09 +0300

    New test

--- src/main/java/Massive2.java
+++ src/main/java/Massive2.java
@@ -1,7 +1,13 @@
+import java.util.Arrays;
+
 public class Massive2 {
     public static void main(String[] args) {
-        int i = 5;
-        double[] myFancyArray = new double[5];
-        System.out.println(myFancyArray[3]);
+        int [] numbers = {1, 2, 3, 4, 5};
+        System.out.println("Исходный массив: " + Arrays.toString(numbers));
+        for (int i = 0; i < numbers.length / 2; i++){
+            int temp = numbers[i];
+            numbers[i] = numbers[numbers.length - 1 - i];
+            numbers[numbers.length - 1 - i] = temp;}
+        System.out.println("Инвертированный массив:" + Arrays.toString(numbers));
     }
     }
