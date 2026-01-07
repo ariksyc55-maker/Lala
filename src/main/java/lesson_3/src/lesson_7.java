3a6399ffe3b1aa336aa385eff0aaa74b24bae03f
User <ariksyc55@gmail.com>
2026-01-07 23:28:57 +0300

    homeTask

---
+++ src/main/java/lesson_3/src/lesson_7.java
@@ -0,0 +1,22 @@
+public class lesson_7 {
+    public static void main(String[] args) {
+        //создаем указаное предложение
+        System.out.println("Hello World! Hi Whooooooosh");
+        String text = "Hello World! Hi Whooooooosh";
+        System.out.println("Длина строки: " + text.length());
+        //здесь задаем пробел разделителем слов
+        String[] words = text.split(" ");
+        //здесь создаем переменную (самое длинное слово)
+        //в дальнейшем внесем сюда слово
+        String longerWord = "";
+        //перебираем слова
+        for (String word : words) {
+            if (word.length() > longerWord.length()) {
+                longerWord = word;
+            }
+        }
+        //вывод самого длинного слова
+        System.out.println("Самое длинное слово: " + longerWord);
+    }
+}
+
