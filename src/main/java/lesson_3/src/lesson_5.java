ad7bc00423d11f0984595ac7d0bc1d4753d941f0
User <ariksyc55@gmail.com>
2026-01-07 22:23:16 +0300

    Lalala

---
+++ src/main/java/lesson_3/src/lesson_5.java
@@ -0,0 +1,9 @@
+public class lesson_5 {
+    public static void main(String[] args) {
+        System.out.print("Дана строка: ");
+        System.out.println(" Java developer ");
+        String text = " Java developer ";
+        System.out.println("Строка без пробелов");
+        System.out.println(text.trim());
+    }
+}
