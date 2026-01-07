513e22a6b0b1cf03b0c0913d721af06d6c268505
User <ariksyc55@gmail.com>
2026-01-07 23:06:50 +0300

    lox

--- src/main/java/lesson_3/src/lesson_6.java
+++ src/main/java/lesson_3/src/lesson_6.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class lesson_6 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст: ");
+        String text = scanner.nextLine();
+        System.out.println(text.replace("Привет", "***"));
+        System.out.println(text.replace("привет", "***"));
     }
 }
