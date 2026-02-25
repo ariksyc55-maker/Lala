2028f97c76d624ed767b8023134c98df3393cf17
User <ariksyc55@gmail.com>
2026-02-25 22:51:44 +0300

    gameUpdate

--- src/main/java/lesson_4/Game.java
+++ src/main/java/lesson_4/Game.java
@@ -42,23 +42,78 @@ public class Game {
 
         if (choice >= 1 && choice <= 6) {
             switch (choice) {
-                case 1: myHero = warriorOne; break;
-                case 2: myHero = warriorTwo; break;
-                case 3: myHero = mageOne;   break;
-                case 4: myHero = mageTwo;  break;
-                case 5: myHero = rogueOne;  break;
-                case 6: myHero = rogueTwo;  break;
+                case 1:
+                    myHero = warriorOne;
+                    break;
+                case 2:
+                    myHero = warriorTwo;
+                    break;
+                case 3:
+                    myHero = mageOne;
+                    break;
+                case 4:
+                    myHero = mageTwo;
+                    break;
+                case 5:
+                    myHero = rogueOne;
+                    break;
+                case 6:
+                    myHero = rogueTwo;
+                    break;
                 default:
                     System.out.println("Неверный выбор! Выбран герой по умолчанию (Артур).");
                     myHero = warriorOne;
             }
             System.out.println("Вы выбрали: " + myHero.name);
+            System.out.println();
         } else {
             System.out.println("Ошибка! Введите число от 1 до 6.");
+            System.out.println();
+            scanner.next();
+        }
+
+        System.out.println("1. " + warriorOne.name + " (ВОИН)");
+        System.out.println("2. " + warriorTwo.name + " (ВОИН)");
+        System.out.println("3. " + mageOne.name + " (МАГ)");
+        System.out.println("4. " + mageTwo.name + " (МАГ)");
+        System.out.println("5. " + rogueOne.name + " (РАЗБОЙНИК)");
+        System.out.println("6. " + rogueTwo.name + " (РАЗБОЙНИК)");
+
+        Hero opponent;
+        scanner.nextInt();
+
+        if (choice >= 1 && choice <= 6) {
+            switch (choice) {
+                case 1:
+                    opponent = warriorOne;
+                    break;
+                case 2:
+                    opponent = warriorTwo;
+                    break;
+                case 3:
+                    opponent = mageOne;
+                    break;
+                case 4:
+                    opponent = mageTwo;
+                    break;
+                case 5:
+                    opponent = rogueOne;
+                    break;
+                case 6:
+                    opponent = rogueTwo;
+                    break;
+                default:
+                    System.out.println("Неверный выбор! Выбран герой по умолчанию (Артур).");
+                    opponent = warriorOne;
+            }
+            System.out.println("Вы выбрали: " + opponent.name);
+            System.out.println();
+        } else {
+            System.out.println("Ошибка! Введите число от 1 до 6.");
+            System.out.println();
+            scanner.next();
         }
     }
-
-
 
     public static void getVoice(Hero hero) {
         hero.voice();
