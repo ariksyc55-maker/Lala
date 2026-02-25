1fe75f5898839b2b756e975ee10a5e8fa6154a3b
User <ariksyc55@gmail.com>
2026-02-25 22:28:11 +0300

    gameUpdate

--- src/main/java/lesson_4/Game.java
+++ src/main/java/lesson_4/Game.java
@@ -4,8 +4,14 @@ import lesson_4.hero.Mage;
 import lesson_4.hero.Rogue;
 import lesson_4.hero.Warrior;
 
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("----- ДОБРО ПОЖАЛОВАТЬ В ИГРУ -----");
+        System.out.println();
 
         Warrior warriorOne = new Warrior("Артур", 200, 45);
 
@@ -19,8 +25,40 @@ public class Game {
 
         Rogue rogueTwo = new Rogue("Брианна", 150, 40);
 
-        attack(warriorOne, mageOne);
+        System.out.println("1. " + warriorOne.name + " (ВОИН)");
+        System.out.println("2. " + warriorTwo.name + " (ВОИН)");
+        System.out.println("3. " + mageOne.name + " (МАГ)");
+        System.out.println("4. " + mageTwo.name + " (МАГ)");
+        System.out.println("5. " + rogueOne.name + " (РАЗБОЙНИК)");
+        System.out.println("6. " + rogueTwo.name + " (РАЗБОЙНИК)");
+
+        System.out.println();
+        System.out.println("Выберите героя для сражения: ");
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        Hero myHero;
+
+        if (choice >= 1 && choice <= 6) {
+            switch (choice) {
+                case 1: myHero = warriorOne; break;
+                case 2: myHero = warriorTwo; break;
+                case 3: myHero = mageOne;   break;
+                case 4: myHero = mageTwo;  break;
+                case 5: myHero = rogueOne;  break;
+                case 6: myHero = rogueTwo;  break;
+                default:
+                    System.out.println("Неверный выбор! Выбран герой по умолчанию (Артур).");
+                    myHero = warriorOne;
+            }
+            System.out.println("Вы выбрали: " + myHero.name);
+        } else {
+            System.out.println("Ошибка! Введите число от 1 до 6.");
+        }
     }
+
+
 
     public static void getVoice(Hero hero) {
         hero.voice();
